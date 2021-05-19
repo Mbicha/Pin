107a809cb0bf6c01a205cb60e3398c58e2363247
Mbicha <mbithicharlse@mail.com>
2021-05-19 13:00:42 +0300

    first commit

---
+++ Pin.py
@@ -0,0 +1,18 @@
+#Pin
+pin = 1234
+
+#Counter
+pin_counter = 0
+
+for pin_counter in range(3):
+    #user Input Prompt
+    enter_pin = int(input('Enter Your Pin'))
+    if enter_pin == pin:
+        print('Pin Correct')
+        exit()
+    else:
+        print('Pin Incorrect')
+        pin_counter +=1
+else:
+    print('Blocked')
+    exit()
